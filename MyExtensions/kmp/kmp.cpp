79052f81de216f759a7e60e1c5129a1eb701a6a0
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-27 23:20:19 -0500

    add kmp

---
+++ MyExtensions/kmp/kmp.cpp
@@ -0,0 +1,75 @@
+#include <Python.h>
+#include <string>
+
+
+static int *getNext(const std::string &pattern) {
+    int i = 0, j = -1;
+    int *next = new int[pattern.size() + 1];
+    next[0] = -1;
+    while (i < pattern.size()) {
+        if (j == -1 || pattern[i] == pattern[j]) {
+            i++;
+            j++;
+            next[i] = j;
+        } else {
+            j = next[j];
+        }
+    }
+    return next;
+}
+
+static int match(const std::string &text, const std::string &pattern) {
+    int *next = getNext(pattern);
+    int i = 0, j = 0;
+    while (i < text.size() && j < pattern.size()) {
+        if (j == -1 || text[i] == pattern[j]) {
+            i++;
+            j++;
+        } else {
+            j = next[j];
+        }
+    }
+    delete[] next;
+    if(j == pattern.size()){
+        return i-j;
+    }
+    return -1;
+}
+
+/* Python Wrapper Functions*/
+/* Destructor function for points */
+static void del_kmp(PyObject *obj) {
+    free(PyCapsule_GetPointer(obj, NULL));
+}
+
+// match
+static PyObject *py_match(PyObject *self, PyObject *args) {
+    const char *text;
+    const char *pattern;
+    if (!PyArg_ParseTuple(args, "OO", &text, &pattern)) {
+        return NULL;
+    }
+    int result = match(text, pattern);
+    return Py_BuildValue("i", result);
+}
+
+/* Module method table */
+static PyMethodDef kmpMethods[] = {
+    {"match", py_match, METH_VARARGS, "Match text with pattern"},
+    {NULL, NULL, 0, NULL}
+};
+
+/* Module definition structure */
+static struct PyModuleDef kmpmodule = {
+    PyModuleDef_HEAD_INIT,  /* m_base */
+    "kmp",                  /* name of module */
+    "kmp",                  /* module documentation, may be NULL */
+    -1,                     /* size of per-interpreter state or -1 */
+    kmpMethods              /* method table */
+};
+
+/* Module initialization function */
+PyMODINIT_FUNC PyInit_kmp(void) {
+    Py_Initialize();
+    return PyModule_Create(&kmpmodule);
+};
