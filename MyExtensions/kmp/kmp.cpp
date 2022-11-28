6750b5a6be9e1a043b59eee578b6abe699f8a1d3
Yisheng Gong <yisheng_gong@onmail.com>
2022-11-28 18:06:09 -0500

    add cache

--- MyExtensions/kmp/kmp.cpp
+++ MyExtensions/kmp/kmp.cpp
@@ -1,8 +1,18 @@
 #include <Python.h>
 #include <string>
 
+struct kmp_next {
+    int *next;
+};
+
+static kmp_next *createNext(int *next){
+    kmp_next *kmp = new kmp_next;
+    kmp->next = next;
+    return kmp;
+}
+
 // get next array
-static int *getNext(const std::string &pattern) {
+static kmp_next *getNext(const std::string &pattern) {
     int i = 0, j = -1;
     int *next = new int[pattern.size() + 1];
     next[0] = -1;
@@ -15,12 +25,13 @@ static int *getNext(const std::string &pattern) {
             j = next[j];
         }
     }
-    return next;
+    return createNext(next);
 }
 
 // match text by pattern
 static int match(const std::string &text, const std::string &pattern) {
-    int *next = getNext(pattern);
+    kmp_next *kmpNext = getNext(pattern);
+    int *next = kmpNext->next;
     int i = 0, j = 0;
     while (i < text.size() && j < pattern.size()) {
         if (j == -1 || text[i] == pattern[j]) {
@@ -37,10 +48,37 @@ static int match(const std::string &text, const std::string &pattern) {
     return -1;
 }
 
+// match with cached next
+static int matchWithNext(const std::string &text, const std::string &pattern, kmp_next *kmpNext) {
+    int *next = kmpNext->next;
+    int i = 0, j = 0;
+    while (i < text.size() && j < pattern.size()) {
+        if (j == -1 || text[i] == pattern[j]) {
+            i++;
+            j++;
+        } else {
+            j = next[j];
+        }
+    }
+    if(j == pattern.size()){
+        return i-j;
+    }
+    return -1;
+}
+
 /* Python Wrapper Functions*/
 /* Destructor function for points */
-static void del_kmp(PyObject *obj) {
-    free(PyCapsule_GetPointer(obj, NULL));
+static void del_kmpnext(PyObject *obj) {
+    free(PyCapsule_GetPointer(obj, "kmp_next"));
+}
+
+/* Utility functions */
+static kmp_next *Pykmp_Askmpnext(PyObject *obj) {
+    return (kmp_next *) PyCapsule_GetPointer(obj, "kmp_next");
+}
+
+static PyObject *Pykmp_Fromkmpnext(kmp_next *next, int must_free) {
+    return PyCapsule_New(next, "kmp_next", must_free ? del_kmpnext : NULL);
 }
 
 // match
@@ -54,9 +92,37 @@ static PyObject *py_match(PyObject *self, PyObject *args) {
     return Py_BuildValue("i", result);
 }
 
+// get cached next
+static PyObject *py_getNext(PyObject *self, PyObject *args) {
+    const char *pattern;
+    if (!PyArg_ParseTuple(args, "O", &pattern)) {
+        return NULL;
+    }
+    kmp_next *next = getNext(pattern);
+    return Pykmp_Fromkmpnext(next, 1);
+}
+
+// match with cached next
+static PyObject *py_matchWithNext(PyObject *self, PyObject *args) {
+    const char *text;
+    const char *pattern;
+    PyObject *py_next;
+    kmp_next *next;
+    if (!PyArg_ParseTuple(args, "OOO", &text, &pattern, &py_next)) {
+        return NULL;
+    }
+    if (!(next = Pykmp_Askmpnext(py_next))) {
+        return NULL;
+    }
+    int result = matchWithNext(text, pattern, next);
+    return Py_BuildValue("i", result);
+}
+
 /* Module method table */
 static PyMethodDef kmpMethods[] = {
+    {"getNext", py_getNext, METH_VARARGS, "Get cached next array"},
     {"match", py_match, METH_VARARGS, "Match text with pattern"},
+    {"matchWithNext", py_matchWithNext, METH_VARARGS, "Match with cached next"},
     {NULL, NULL, 0, NULL}
 };
 
